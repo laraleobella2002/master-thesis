d93396088ad3d28cb27af37e013847f8873a242f
laraleobella2002 <lara.amarna18@gmail.com>
2026-02-16 13:21:05 +0100

    Add files via upload

---
+++ really last version.py
@@ -0,0 +1,195 @@
+from psychopy import visual, event, core, monitors, gui
+import random
+import csv
+import os
+
+step_size = 0.05
+min_value= 0.03
+num_trials = 60
+lineHight= 1.5
+lineThickness = 2
+stim_time= 0.10
+streak_left = 0
+streak_right = 0
+
+block_conditions = [
+    {'label':'Block 1: Binocular_UC', 'spacing': 0, 'crowdCount': 0, 'eccentricity': 8},
+    {'label':'Block 2: Binocular_C', 'spacing': 0.5, 'crowdCount': 2, 'eccentricity': -8},
+    {'label':'Block 3: Monocular_UC', 'spacing': 0, 'crowdCount': 0, 'eccentricity': 5},
+    {'label':'Block 4: Monocular_C', 'spacing': 0.5, 'crowdCount': 2, 'eccentricity': -5},
+]
+
+expInfo={ 'participantID': ''}
+dlg = gui.DlgFromDict(dictionary=expInfo, title='Vernier Experiment')
+participant_name = expInfo['participantID']
+
+
+fileName= f'data_{participant_name}.csv'
+data_file = open(fileName, 'w', newline='')
+writer = csv.writer(data_file)
+writer.writerow(['label', 'trials', 'offset', 'keys', 'direction', 'response_time', 'statement','response_numeric'])
+
+mon = monitors.Monitor('tempMonitor')
+mon.setWidth(53)
+mon.setDistance(57.9)
+mon.setSizePix([1920, 1080])
+
+win = visual.Window(fullscr=True, color='white', units='deg', monitor=mon)
+win.mouseVisible = False
+rt_clock = core.Clock()
+first_page = visual.TextStim(win, text='Hello\n\n1. Please keep looking at the black cross during the trials.\n2. If the upper line is to the right, press →\n3. If it is to the left, press ←\n\nPress SPACE to start :)', color='black')
+first_page.draw()
+win.flip()
+event.waitKeys(keyList=['space'])
+
+for condition in block_conditions:
+    streak_left = 0
+    streak_right = 0
+    current_spacing = condition['spacing']
+    current_crowdCount = condition['crowdCount']
+    current_eccentricity = condition['eccentricity']
+    block_label = condition['label']
+    start_value_right = 0.2
+    start_value_left = -0.2
+    msg = f"Starting {block_label}\n Press SPACE to continue."
+    block_msg = visual.TextStim(win, text=msg, color='black')
+    block_msg.draw()
+    win.flip()
+    event.waitKeys(keyList=['space'])
+    
+   
+    for trials in range(num_trials):
+        staircase = random.choice(['left', 'right'])
+        if staircase == 'left':
+            offset = start_value_left
+            direction = 'left'
+        else:
+            offset = start_value_right
+            direction = 'right'
+           
+        vernier1 = visual.Line(
+            win=win,
+            lineColor='black',
+            lineWidth=lineThickness,
+            start=(current_eccentricity+ (offset), lineHight),
+            end=(current_eccentricity+ (offset), 0)
+            )
+
+        vernier2 = visual.Line(
+            win=win,
+            lineColor='black',
+            lineWidth=lineThickness,
+            start=(current_eccentricity, 0),
+            end=(current_eccentricity, -lineHight)
+            )
+            
+        cross1 = visual.Line(
+            win=win,
+            lineColor='black',
+            lineWidth=4,
+            start=(0,-0.3),
+            end=(0,0.3)
+            )
+           
+        cross2= visual.Line(
+            win=win,
+            lineColor='black',
+            lineWidth=4,
+            start=(-0.3, 0),
+            end=(0.3,0)
+            )
+
+        flankers = []
+        for i in range(-current_crowdCount, current_crowdCount + 1):
+            if i == 0:
+                continue
+            x_pos = current_eccentricity + (i * current_spacing)
+            if x_pos==offset:  
+                continue
+               
+            if offset<0:
+                direction='left'
+            else:
+                direction='right'
+
+            flanker = visual.Line(
+                win=win,
+                lineColor='black',
+                lineWidth=lineThickness,
+                start=(x_pos, -lineHight),
+                end=(x_pos, lineHight)
+                )
+            flankers.append(flanker)
+            
+        for flanker in flankers:
+            flanker.draw()
+
+        vernier1.draw()
+        vernier2.draw()
+        cross1.draw()
+        cross2.draw()
+        win.flip()
+        cross1.draw()
+        cross2.draw()
+        core.wait(stim_time)
+        win.flip()
+        cross1.draw()
+        cross2.draw()
+        rt_clock.reset()
+        keys = event.waitKeys(keyList=['right', 'left','escape'])
+        response_time = rt_clock.getTime()
+
+        if 'escape' in keys:
+            win.close()
+            core.quit()
+
+        key_pressed = keys[0]
+        if key_pressed == direction:
+            statement = 'correct'
+            response_numeric = 1
+            feedback_color= 'green'
+            if staircase == 'left':
+                streak_left+=1
+                if streak_left==2:
+                    start_value_left+= step_size
+                    streak_left=0
+                    if start_value_left > -min_value:
+                        start_value_left = -min_value
+            else:
+                streak_right+=1
+                if streak_right==2:
+                    start_value_right -= step_size
+                    streak_right=0
+                    if start_value_right < min_value:
+                        start_value_right = min_value
+               
+        else:
+            statement = 'false'
+            feedback_color= 'red'
+            response_numeric = 0
+            if staircase == 'left':
+                start_value_left -= step_size
+                streak_left=0
+            else:
+                start_value_right += step_size
+                streak_right=0
+        writer.writerow([block_label, trials, offset, keys, direction, response_time, statement, response_numeric])
+        
+        cross1.lineColor = feedback_color
+        cross2.lineColor = feedback_color
+        cross1.draw()
+        cross2.draw()
+        win.flip()
+        core.wait(0.5)
+        cross1.lineColor = 'black'
+        cross2.lineColor = 'black'
+        
+        
+
+last_page= visual.TextStim(win, text= 'Thank you, press escape to exit' , color='black')
+last_page.draw()
+win.flip()
+event.waitKeys(keyList=['escape'])
+data_file.close()
+win.close()
+core.quit()
